8cd37272185bc47feac359a87a4d54c1631c8e00
Simon Walker <s.r.walker101@googlemail.com>
2017-07-20 17:58:00 +0100

    Minor formatting

--- imagenet.py
+++ imagenet.py
@@ -1,18 +1,12 @@
 
 # coding: utf-8
 
-# In[ ]:
 
-
-from keras.layers import *
-from keras.models import Sequential, Model
 from keras.callbacks import ModelCheckpoint
-from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-import h5py
 import logging
 import currentmodel
 
@@ -21,56 +15,23 @@ logger = logging.getLogger('imagenet')
 
 N_EPOCHS = 50
 
-# In[ ]:
-
-
 logger.info('Reading labels')
 train_labels = pd.read_csv('train_labels.csv')
 train_labels.head()
 
-
-# In[ ]:
-
-
 y = train_labels.invasive.values
-# y = to_categorical(y)
-
-
-# In[ ]:
-
 
 logger.info('Loading features')
 X = np.load('train.npy')
 
-
-# In[ ]:
-
-
 assert X.shape[0] == y.shape[0]
 
-
-# In[ ]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-
-# In[ ]:
-
-
-# In[ ]:
 
 logger.info('Setting up model')
 model = currentmodel.build_model()
 
-# In[ ]:
-
-
 model.summary()
-
-
-# In[ ]:
-
 
 logger.info('Building image data generator')
 train_datagen = ImageDataGenerator(
@@ -80,16 +41,12 @@ train_datagen = ImageDataGenerator(
     horizontal_flip=True)
 train_datagen.fit(X_train)
 
-
-# In[ ]:
-
-
 logger.info('Fitting model')
 model.fit_generator(
     train_datagen.flow(X_train, y_train, batch_size=32),
     steps_per_epoch=X_train.shape[0] // 32,
     epochs=N_EPOCHS,
     validation_data=(X_test, y_test),
-    callbacks=[ModelCheckpoint('VGG16-transferlearning.model', monitor='val_acc',
-        save_best_only=True)],
+    callbacks=[ModelCheckpoint('VGG16-transferlearning.model',
+                               monitor='val_acc', save_best_only=True)],
 )
