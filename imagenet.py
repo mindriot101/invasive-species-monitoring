3d3d0d3f9be895b3bfb9104273b34705305ddd06
Simon Walker <s.r.walker101@googlemail.com>
2017-07-20 17:44:36 +0100

    Train all layers

--- imagenet.py
+++ imagenet.py
@@ -71,9 +71,6 @@ add_model.add(Flatten(input_shape=base_model.output_shape[1:]))
 add_model.add(Dense(256, activation='relu'))
 add_model.add(Dense(1, activation='sigmoid'))
 
-for layer in base_model.layers:
-    layer.trainable = False
-
 model = Model(inputs=base_model.input, outputs=add_model(base_model.output))
 model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
